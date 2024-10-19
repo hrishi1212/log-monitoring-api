b97d8086d9bacc0253390a2200abb59da6f4a925
hrishikesh kale <hrishikeshkale@hrishikeshs-MacBook-Air.local>
2024-10-18 20:27:17 -0600

    Refacotr- added description to methods

--- src/services/logService.ts
+++ src/services/logService.ts
@@ -1,14 +1,14 @@
 import { statSync } from "fs";
 import config from "config";
 import * as path from "path";
-import * as os from 'os';
+import * as os from "os";
 import { InvalidParameterError, NotFoundError } from "../errors/customError";
-import { open } from "fs/promises";
+import { FileHandle, open } from "fs/promises";
 
 const LOG_DIR: string = config.get<string>("logDir");
 
 /**
- * This function calculates the chunk size based on 1% of the free system memory. 
+ * This function calculates the chunk size based on 1% of the free system memory.
  * The chunk size is capped between a minimum of 64KB and a maximum of 256KB.
  * we can adjust these limits based on your use case and performance testing.
  * @returns chunk size in bytes
@@ -16,10 +16,12 @@ const LOG_DIR: string = config.get<string>("logDir");
 const getDynamicChunkSize = (): number => {
   // get free from system
   const freeMemory = os.freemem();
-  const chunkSize = Math.min(Math.max(freeMemory * 0.01, 64 * 1024), 256 * 1024); 
+  const chunkSize = Math.min(
+    Math.max(freeMemory * 0.01, 64 * 1024),
+    256 * 1024
+  );
   return Math.floor(chunkSize);
 };
-
 
 /**
  * Retrieves log entries from a file, with optional filtering by keyword and limiting the number of entries.
@@ -44,7 +46,9 @@ export const retrieveLogs = async (
     await readLogFile(fd, fileSize, keyword, maxEntries, logEntries);
 
     if (keyword && logEntries.length === 0) {
-      throw new InvalidParameterError(`No log entries found matching the keyword "${keyword}"`);
+      throw new InvalidParameterError(
+        `No log entries found matching the keyword "${keyword}"`
+      );
     }
 
     await fd.close();
@@ -56,6 +60,9 @@ export const retrieveLogs = async (
 
 /**
  * Gets the size of the log file.
+ *
+ * @param filePath - The path to the log file.
+ * @returns The size of the log file in bytes.
  */
 const getFileSize = (filePath: string): number => {
   const fileStat = statSync(filePath);
@@ -64,9 +71,15 @@ const getFileSize = (filePath: string): number => {
 
 /**
  * Reads the log file in reverse order, processing chunks and filtering by keyword.
+ *
+ * @param fd - The file descriptor for the log file.
+ * @param fileSize - The size of the log file in bytes.
+ * @param keyword - (Optional) A keyword to filter the log entries by.
+ * @param maxEntries - (Optional) The maximum number of log entries to retrieve.
+ * @param logEntries - An array to store the filtered log entries.
  */
 const readLogFile = async (
-  fd: any,
+  fd: FileHandle,
   fileSize: number,
   keyword: string | undefined,
   maxEntries: number | undefined,
@@ -92,6 +105,12 @@ const readLogFile = async (
 
 /**
  * Reads a chunk of data from the file at a given position.
+ *
+ * @param fd - The file descriptor for the file.
+ * @param buffer - The buffer to read the data into.
+ * @param bytesToRead - The number of bytes to read from the file.
+ * @param position - The position in the file to start reading from.
+ * @returns A promise that resolves to the string representation of the read data.
  */
 const readChunk = async (
   fd: any,
@@ -103,9 +122,13 @@ const readChunk = async (
   return buffer.toString("utf-8", 0, bytesRead);
 };
 
-
 /**
- * Processes log lines and filters them based on the keyword.
+ * Processes log lines and filters them based on the provided keyword.
+ *
+ * @param lines - An array of log lines to process.
+ * @param keyword - An optional keyword to filter the log lines by.
+ * @param logEntries - An array to store the filtered log entries.
+ * @param maxEntries - An optional maximum number of log entries to retrieve.
  */
 const processLogLines = (
   lines: string[],
@@ -119,13 +142,18 @@ const processLogLines = (
       logEntries.push(line);
       if (maxEntries && logEntries.length >= maxEntries) {
         break;
-      } 
+      }
     }
   }
 };
 
 /**
  * Handles specific error cases and rethrows unexpected errors.
+ *
+ * @param err - The error object to handle.
+ * @param filename - The filename associated with the error.
+ * @throws {NotFoundError} If the error code is "ENOENT", indicating the log file was not found.
+ * @throws {Error} Rethrows any unexpected errors.
  */
 const handleErrors = (err: any, filename: string) => {
   if (err.code === "ENOENT") {
